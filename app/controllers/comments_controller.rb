8648b75186ff7a091b1144b9aee76c67c0bfef17
ruru8 <chiharu.yasuda@classi.jp>
2019-06-13 15:26:02 +0900

    add comments controller and edit model

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,39 @@
+class CommentsController < ApplicationController
+  before_action :load_commentable
+
+  def index
+    @comments = @commentable.comments
+  end
+
+  def new
+    @comments = @commentable.comments.new
+  end
+
+  def create
+    @comment = @commentable.comments.create(comment_params)
+    redirect_to @commentable notice: 'Comment was successfully created.'
+  end
+
+  def destroy
+    @comment = @commentable.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @commentable notice: 'Comment was successfully destroyed.'
+  end
+
+
+
+  private
+  # URLからBookかReportを取得する
+  # ex: /books/:id/comments
+  # ex: /reports/:id/comments
+  def load_commentable
+    resource, id = request.path.split('/')[1, 2]
+    @commentable = resource.singularize.classify.constantize.find(id)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:name, :body)
+  end
+
+
+end
