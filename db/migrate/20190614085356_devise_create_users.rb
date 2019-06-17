3c3e7987fe6bbbff91a3f8055d854190f2ee5569
ruru8 <chiharu.yasuda@classi.jp>
2019-06-17 17:47:11 +0900

    DBフイールド追加

--- db/migrate/20190614085356_devise_create_users.rb
+++ db/migrate/20190614085356_devise_create_users.rb
@@ -3,6 +3,11 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      # 追記事項
+      t.string :name, null: false
+      t.text :profile
+      t.string :zip
+      t.text :address
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,10 @@ ActiveRecord::Schema.define(version: 2019_06_14_085356) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "profile"
+    t.string "zip"
+    t.text "address"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
