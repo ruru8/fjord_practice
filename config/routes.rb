c9a88355d6ccfffcd4d17e3b536908d4d5fa2c09
ruru8 <chiharu.yasuda@classi.jp>
2019-06-18 11:23:02 +0900

    プロフィールshowページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
+  resources :users, only: [:show]
   resources :books do 
     resources :comments, only: [:create, :destroy ]
   end
