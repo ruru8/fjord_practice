c20f8ced9af4e1eb05ae61e8ab4e537c9deb958c
ruru8 <chiharu.yasuda@classi.jp>
2019-06-21 18:08:21 +0900

    SytemTest BooksTest created

---
+++ test/system/books_test.rb
@@ -0,0 +1,48 @@
+require "application_system_test_case"
+
+class BooksTest < ApplicationSystemTestCase
+  def setup
+    @book = books(:one)
+    @user = users(:one)
+    log_in(@user)
+  end
+
+  test "visiting the index" do
+    visit books_url
+    assert_selector "h1", text: "Books"
+  end
+
+  test "creating a Book" do
+    visit books_url
+    click_on "New"
+
+    fill_in "book[title]", with: @book.title
+    fill_in "book[memo]", with: @book.memo
+    click_on "create"
+
+    assert_text "create"
+    click_on "Back"
+  end
+
+  test "updating a Book" do
+    visit books_url
+    first('tbody tr').click_on 'Edit'
+
+    fill_in "book[title]", with: @book.title
+    fill_in "book[memo]", with: @book.memo
+
+    click_on "update"
+
+    assert_text "updated"
+    click_on "Back"
+  end
+
+  test "destroying a Book" do
+    visit books_url
+    page.accept_confirm do
+      first('tbody tr').click_on 'Delete'
+    end
+
+    assert_text "destroyed"
+  end
+end
