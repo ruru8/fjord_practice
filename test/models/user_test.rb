1967c2e770ceb3c19ce1b6e53cb31b75778c331e
ruru8 <chiharu.yasuda@classi.jp>
2019-07-01 16:44:36 +0900

    不要なモデルテストの消去、Userモデルテストにメソッドテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "minitest/autorun"
+require "omniauth"
 
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = users(:user1)
+    @user2 = users(:user2)
   end
 
   test "name should be present" do
@@ -15,5 +18,41 @@ class UserTest < ActiveSupport::TestCase
   test "profile should not be too long" do
     @user.profile = "a" * 201
     assert_not @user.valid?
+  end
+
+  test "user should be registered as it is when users have the same account as auth" do
+    user = User.new(provider: "test", uid: "aaa")
+    auth = User.find_for_github_oauth(user)
+    assert @user == auth
+  end
+
+  test "user should be created when users don't have the same account as auth" do
+    auth = OmniAuth::AuthHash.new(provider: "github",
+                                  uid: "123",
+                                  info: { name: "tanaka" })
+    user = User.find_for_github_oauth(auth)
+    assert user == User.find(user.id)
+  end
+
+  test "emaill should be created using uid and prober" do
+    auth = users(:user_github)
+    auth_email = User.dummy_email(auth)
+    assert_equal "12345-github@example.com", auth_email
+  end
+
+  test "user should be added to followees" do
+    @user.follow(@user2)
+    assert @user.followees.include?(@user2)
+  end
+
+  test "user should be deleted from followees" do
+    @user.followees << @user2
+    @user.unfollow(@user2)
+    assert_not @user.followees.include?(@user2)
+  end
+
+  test "should return true if user is in followees" do
+    @user.followees << @user2
+    assert_equal @user.following?(@user2), true
   end
 end
